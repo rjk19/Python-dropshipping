75ce4c3cf89f3397ab6bfe0efc90c4f610bc1771
ttonjann <64033648+ttonjann@users.noreply.github.com>
2021-10-28 14:38:15 +0200

    Insert function

---
+++ SQL/insertData function.py
@@ -0,0 +1,31 @@
+def insert_varibles_into_table(naam, prijs,beschrijving, features):
+    try:
+        mydb = mysql.connector.connect(
+            host = "localhost",
+            # port = "3306", # Voor windows
+            port = "8889", # Voor Mac
+            user = "root",
+            # password = "", # Voor windows
+            password = "root", # Voor Mac
+            database = "BigD"
+        )
+        cursor = mydb.cursor()
+        mysql_insert = "INSERT INTO Product (naam, prijs, beschrijving, features) VALUES (%s, %s, %s, %s)"
+
+        product = (naam, prijs, beschrijving, features)
+        cursor.execute(mysql_insert, product)
+        mydb.commit()
+        print("Product inserted successfully")
+    
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+
+    finally:
+        if mydb.is_connected():
+            cursor.close()
+            mydb.close()
+            print("MySQL connection is closed")
+
+
+for i in list:
+    insert_varibles_into_table(i.naam, i.prijs, i.beschrijving, i.features)
